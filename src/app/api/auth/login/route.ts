e846af12541c4426d623ac5ec2a1155a079bda53
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-11-07 18:31:23 +0500

    bug fixed

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
 
     if (username === 'user' && password === 'password') {
         const token = 'example-token';
-        const response = NextResponse.json({ success: true, });
+        const response = NextResponse.json({ success: true, token: token });
 
         response.cookies.set('new-token', token, { path: '/', httpOnly: true, maxAge: 60 * 2 });
 
